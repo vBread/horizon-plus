3b641424e831a6e396923e3305c56987974cc465
K. Akatsuki <thevortexspirit@gmail.com>
2021-03-04 21:54:36 -0500

    feat: add theming to gql

---
+++ src/languages/graphql.ts
@@ -0,0 +1,63 @@
+import { TokenColor } from '../types';
+
+export default {
+	keyword: {
+		'operation, on, fragment, type, schema, enum, scalar, interface, implements, union, input': {
+			graphql: {
+				foreground: TokenColor.Amethyst
+			}
+		}
+	},
+
+	string: {
+		unquoted: {
+			alias: {
+				graphql: {
+					foreground: TokenColor.Carmine
+				}
+			}
+		}
+	},
+
+	variable: {
+		graphql: {
+			foreground: TokenColor.Carmine
+		},
+
+		arguments: {
+			graphql: {
+				foreground: TokenColor.Carmine
+			}
+		},
+
+		fragment: {
+			graphql: {
+				foreground: TokenColor.Nectarine
+			}
+		}
+	},
+
+	constant: {
+		character: {
+			enum: {
+				graphql: {
+					foreground: TokenColor.Carmine
+				}
+			}
+		}
+	},
+
+	entity: {
+		scalar: {
+			foreground: TokenColor.Nectarine
+		}
+	},
+
+	punctuation: {
+		'colon, comma, assignment, or': {
+			graphql: {
+				foreground: TokenColor.Amethyst
+			}
+		}
+	}
+};
