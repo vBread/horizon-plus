3722836e4e74a896777a4e0d857d1130112a52f3
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-14 16:05:25 -0400

    feat(languages): add Lua support

---
+++ src/languages/lua.ts
@@ -0,0 +1,17 @@
+/**
+ * Scopes based on "Lua"
+ *
+ * https://marketplace.visualstudio.com/items?itemName=sumneko.lua
+ */
+
+import { TokenColor } from '../types';
+
+export default {
+	keyword: {
+		local: {
+			lua: {
+				foreground: TokenColor.Amethyst
+			}
+		}
+	}
+};
