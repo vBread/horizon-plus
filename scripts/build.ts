9f840061c4229a725943dee0d3201f7e44e6c64e
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-19 20:36:42 -0400

    refactor: rewrite

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,86 +1,57 @@
 import { writeFile } from 'fs/promises';
-import { basename } from 'path';
-import readdirp from 'readdirp';
-import { TokenConfig } from '../src/types';
-import { walkObject } from './index';
+import { renderSync } from 'sass';
 
-const languages = readdirp(`${process.cwd()}/src/languages`);
+const index = `${process.cwd()}/src/languages/index.scss`;
 const themeFile = `${process.cwd()}/themes/Orchid-color-theme.json`;
-const semanticTokenColors: Record<string, TokenConfig['settings']> = {};
+
+interface Token {
+	scope: string;
+	settings: {
+		foreground?: string;
+		fontStyle?: string;
+	};
+}
+
+interface MatchGroups {
+	scopes: string;
+	foreground?: string;
+	fontStyle?: string;
+	style: string;
+}
+
+const tokens: Token[] = [];
 
 void (async (): Promise<void> => {
-	const buildScopes = (object: object): TokenConfig[] => {
-		const tokens = new Map<string, Record<string, string>>();
-		const tokenColors: TokenConfig[] = [];
+	const compiled = renderSync({ file: index }).css.toString();
 
-		for (const path of walkObject(object)) {
-			const keys = path.split('.');
-			const cleaned: string[] = [];
-			const [setting, value] = keys.splice(-2);
+	let match: RegExpExecArray;
+	const regex = /(?<scopes>.+) {\s+(?:(?<foreground>color)|(?<fontStyle>font-(?:weight|style))): (?<style>#[a-f\d]{6})/gim;
 
-			for (let i = 0; i < keys.length; i++) {
-				const previous = keys[i - 1];
-				const current = keys[i];
+	while ((match = regex.exec(compiled))) {
+		const groups = (match.groups as unknown) as MatchGroups;
+		const settings: Token['settings'] = {};
 
-				if (current.startsWith('\\')) {
-					cleaned.push(`${previous}${current.replace('\\', '')}`);
-					cleaned.splice(cleaned.indexOf(previous), 1);
-
-					continue;
-				}
-
-				cleaned.push(current);
-			}
-
-			const scope = cleaned.join('.');
-			const scopes: string[] = [];
-
-			const multi = cleaned.find((scope) => /\w+, \w+/.test(scope));
-
-			if (multi) {
-				multi.split(', ').forEach((s) => {
-					const parent = scope.split(/.\w+,/)[0];
-
-					return scopes.push(`${parent === '' ? '' : `${parent}.`}${s}`);
-				});
-			} else {
-				scopes.push(scope);
-			}
-
-			for (const scope of scopes) {
-				if (tokens.has(scope)) {
-					tokens.get(scope)[setting] = value;
-				} else {
-					tokens.set(scope, { [setting]: value });
-				}
-			}
+		if (groups.foreground) {
+			settings.foreground = groups.style;
 		}
 
-		for (const [scope, settings] of tokens) {
-			tokenColors.push({ scope, settings });
+		if (groups.fontStyle) {
+			settings.fontStyle = groups.style;
 		}
 
-		return tokenColors;
-	};
-
-	const tokenColors: TokenConfig[] = [];
-
-	for await (const file of languages) {
-		const scopes = (await import(file.fullPath)).default;
-
-		if (basename(file.basename, '.ts') === 'semantic') {
-			for (const token of buildScopes(scopes)) {
-				semanticTokenColors[token.scope as string] = token.settings;
-			}
+		if (groups.scopes.includes(', ')) {
+			groups.scopes.split(', ').map((scope) => {
+				return tokens.push({ scope: scope.replace(/#/g, ''), settings });
+			});
 
 			continue;
 		}
 
-		tokenColors.push(...buildScopes(scopes));
+		tokens.push({ scope: groups.scopes.replace(/#/g, ''), settings });
 	}
 
 	let theme = (await import(themeFile)).default;
-	theme = { ...theme, tokenColors, semanticTokenColors };
+	theme = { ...theme, tokenColors: tokens };
 
 	writeFile(themeFile, JSON.stringify(theme, null, 4));
 })();
